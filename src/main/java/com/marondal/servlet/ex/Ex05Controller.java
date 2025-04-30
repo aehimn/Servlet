f5d8fa3c151938e8c0c7d8eb9cd6d3cae0122d99
haemin <heaminkim1007@gmail.com>
2025-04-30 18:45:56 +0900

    [김해민] - post method 일부 예재 작성

--- src/main/java/com/marondal/servlet/ex/Ex05Controller.java
+++ src/main/java/com/marondal/servlet/ex/Ex05Controller.java
@@ -6,8 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Ex05Controller extends HttpServlet {
 	
+	// 파라미터가
+	// 보안에 민감한 데이터가 포함되어 있는지
+	// 긴 데이터가 포함되어 있는지
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+	
+		// 이름과 생년월일을 전달받고,
+		// 이름과 나이를 html로 구성
 		
 	}
 
